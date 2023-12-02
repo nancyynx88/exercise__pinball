2f6c0cf94094dc4ec8d2db26432fdb4a5e735eab
Nancy Y <naixin.yang@gmail.com>
2023-12-01 18:01:57 -0600

    shows locations in box

--- pinball/src/App.js
+++ pinball/src/App.js
@@ -10,13 +10,17 @@ function App() {
 
   const fetchLocations = async (latitude, longitude) => {
     if (!latitude || !longitude) {
-      setError("Latitude and longitude are required");
+      setError('Latitude and longitude are required');
       return;
     }
 
     try {
       // Fetch the closest region based on latitude and longitude
-      const regionResponse = await fetch(`https://pinballmap.com/api/v1/regions/closest_by_lat_lon.json?lat=${encodeURIComponent(latitude)}&lon=${encodeURIComponent(longitude)}`);
+      const regionResponse = await fetch(
+        `https://pinballmap.com/api/v1/regions/closest_by_lat_lon.json?lat=${encodeURIComponent(
+          latitude
+        )}&lon=${encodeURIComponent(longitude)}`
+      );
       if (!regionResponse.ok) {
         throw new Error(`HTTP error! status: ${regionResponse.status}`);
       }
@@ -24,30 +28,43 @@ function App() {
 
       // Fetch locations in the found region
       if (regionData && regionData.region && regionData.region.name) {
-        const locationsResponse = await fetch(`https://pinballmap.com/api/v1/region/${regionData.region.name}/locations.json`);
+        const locationsResponse = await fetch(
+          `https://pinballmap.com/api/v1/region/${regionData.region.name}/locations.json`
+        );
         if (!locationsResponse.ok) {
           throw new Error(`HTTP error! status: ${locationsResponse.status}`);
         }
         const locationsData = await locationsResponse.json();
         setLocations(locationsData.locations);
       } else {
-        throw new Error("Region data is not available");
+        throw new Error('Region data is not available');
       }
     } catch (error) {
       setError(`Error fetching locations: ${error.message}`);
-      console.error("Error: ", error);
+      console.error('Error: ', error);
     }
   };
 
   return (
     <div>
-      <div className="app-container">
+      <div className='App-container'>
         <Typography variant='h4' component='h1' gutterBottom>
-          Find the Closest Pinball Near Me
+          Pinball Finder: Discover Nearby Loctions
         </Typography>
+        <div className='instruction-text'>
+          <p>
+            Enter coordinates (latitude -90 to 90, longitude -180 to 180) and
+            click 'Search'.
+          </p>
+          <p>Or use 'Near Me' to auto-fill with your current location.</p>
+        </div>
       </div>
+
       <Input fetchLocations={fetchLocations} />
-      <Locations locations={locations} />
+      <div className = 'locaiton-box'>
+        <Locations locations={locations} />
+      </div>
+
       {error && <div>Error: {error}</div>}
     </div>
   );
--- pinball/src/Locations.js
+++ pinball/src/Locations.js
@@ -3,9 +3,12 @@ import './Locations.css';
 
 export default function Locations({ locations }) {
   console.log("Locations in component in locations.js:", locations); 
-
+  if (locations.length === 0) {
+    return null; // Don't render the container if there are no locations
+  }
   return (
     <div className="locations-container">
+      <h2 className="locations-title">Nearby Pinball Locations</h2>
       {locations.map((location, index) => (
         <div key={index} className="location-item">
           {/* Display location details here */}
